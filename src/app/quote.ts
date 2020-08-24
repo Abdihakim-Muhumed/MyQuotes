3ba746a1323805cc16c71e82185913476d9c2f29
“Abdihakim <abdihakim.0017@gmail.com.comgit config --global user.name abdihakim17git config --global user.email abdihakim.0017@gmail.com.com>
2020-08-24 17:54:18 +0300

    Add custom pipe to calculate how long a quote lasted

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -2,7 +2,7 @@ export class Quote {
     public likes: number;
     public  dislikes: number;
     public showDetails: boolean;
-    constructor(public id:number, public userName:string, public quotation:string, public author: string){
+    constructor(public id:number, public userName:string, public quotation:string, public author: string,public quoteDate: Date){
         this.likes = 0;
         this.dislikes = 0;
         this.showDetails= false;
