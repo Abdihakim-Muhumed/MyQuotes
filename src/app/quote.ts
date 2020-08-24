af1288590039e235e147e4590a2c5426ac9c774d
“Abdihakim <abdihakim.0017@gmail.com.comgit config --global user.name abdihakim17git config --global user.email abdihakim.0017@gmail.com.com>
2020-08-24 12:58:55 +0300

    Add like and dislike buttons

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,7 +1,7 @@
 export class Quote {
     public likes: number;
     public  dislikes: number;
-    constructor(public id:number, public userName:string, public quotation:string){
+    constructor(public id:number, public userName:string, public quotation:string, public author: string){
         this.likes = 0;
         this.dislikes = 0;
     }
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,10 +9,16 @@ import { Quote } from '../quote';
 export class QuotesComponent implements OnInit {
 
   quotes: Quote [] =[
-    new Quote(1,'Abdi@99','Education is your life, guard it well'),
-    new Quote(2,'Ali-254','Say the truth even if it i bitter'),
-    new Quote(3,'Fekir-11','Better late than  never')
+    new Quote(1,'Abdi@99','Education is your life, guard it well','Ali'),
+    new Quote(2,'Ali-254','Say the truth even if it i bitter','Prophet Muhammad'),
+    new Quote(3,'Fekir-11','Better late than  never','Englis Wiseman')
   ]
+  addLike(index){
+    this.quotes[index].likes=+1;
+  }
+  addDislike(index){
+    this.quotes[index].dislikes=+1;
+  }
   constructor() { }
 
   ngOnInit(): void {
