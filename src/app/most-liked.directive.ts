7bf33e1984225a178a3d4898b63b8ac1cad8120e
“Abdihakim <abdihakim.0017@gmail.com.comgit config --global user.name abdihakim17git config --global user.email abdihakim.0017@gmail.com.com>
2020-08-24 16:51:32 +0300

    Add custome directive most-liked

---
+++ src/app/most-liked.directive.ts
@@ -0,0 +1,13 @@
+import { Directive, ElementRef} from '@angular/core';
+
+@Directive({
+  selector: '[appMostLiked]'
+})
+export class MostLikedDirective {
+
+  constructor(private elem:ElementRef){
+    this.elem.nativeElement.style.textDecoration='line-through';
+  }
+
+
+}
