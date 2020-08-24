ffd87c10718bc760be39995663e5bc55c6c3ae34
“Abdihakim <abdihakim.0017@gmail.com.comgit config --global user.name abdihakim17git config --global user.email abdihakim.0017@gmail.com.com>
2020-08-24 11:29:45 +0300

    Add class properties

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,8 @@
 export class Quote {
+    public likes: number;
+    public  dislikes: number;
+    constructor(public id:number, public userName:string, public quotation:string){
+        this.likes = 0;
+        this.dislikes = 0;
+    }
 }
